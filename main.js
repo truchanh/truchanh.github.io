6c177bb061d8b6152d84b996831b09ba770d3034
truchanh <73228138+truchanh@users.noreply.github.com>
2021-04-03 19:11:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let btn = document.querySelector("#change-color");
-let txt = document.querySelector("h1");//Hello World
+let txt = document.querySelector("#h1-heading");//Hello World
 let selectValue = () => {
   return Math.floor(Math.random()*256);
 }
 let randomColor = function() {
-  let randColor = `rgb(${selectValue()},${selectValue()},${selectValue()})`;
+  let randColor = 'rgb('+selectValue() + ',' + selectValue() + ',' + selectValue()+')';
   txt.style.color = randColor;
 }
 btn.onclick = randomColor;
